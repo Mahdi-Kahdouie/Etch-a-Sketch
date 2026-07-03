db3a2948f96fff8f186005841c106b6ab6bcb151
Mahdi Kahdouie <Mahdi.Kahdouie@gmail.com>
2026-07-03 18:35:46 +0330

    Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid.
    Part 4 of exercise has been done

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
+
 const container = document.querySelector("#container");
 
-container.innerHTML = "";
+
 for (let i = 0; i < 256; i++) {
     
     const square =document.createElement("div")
@@ -11,3 +12,31 @@ for (let i = 0; i < 256; i++) {
     container.appendChild(square);
     
 }
+
+const page = document.querySelector("#page");
+const btnNum =document.createElement("button")
+page.appendChild(btnNum);
+btnNum.textContent="Reset";
+btnNum.addEventListener('click',(event) => {
+            let num = prompt("Please enter number of squares per side:");
+            if (num>100 || num==null || num=="")
+                alert("Please enter number between 1 to 100 ")
+            else
+            {
+                container.innerHTML="";
+                for (let i = 0; i < num*num; i++) {
+    
+                const square =document.createElement("div")
+                square.className="squareShape";
+                square.style.width=(100/num)+"%";
+                square.style.height=(100/num)+"%";
+                square.addEventListener('mouseenter',(event) => {
+                        square.style.backgroundColor="red";
+                    });
+                container.appendChild(square);
+    
+}
+            }
+    });
+
+
